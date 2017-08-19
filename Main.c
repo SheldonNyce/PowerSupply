26db9f1b6b7b89435f6a024fbb53492435022c58
Andrew Paterson <andrew.ian.paterson@gmail.com>
2017-08-19 16:37:19 +0200

    Minimal deployable project.

---
+++ Main.c
@@ -0,0 +1,25 @@
+/*
+ * File:   Main.c
+ * Author: andrew
+ *
+ * Created on 17 August 2017, 1:45 PM
+ */
+
+
+#include <stdio.h>
+#include "xc.h"
+
+int main(void) 
+{
+    int i;
+    int j = 0;
+    
+    for (i = 0; i < 100; i++)
+    {
+        j = j + i;
+    }
+        
+    printf("%i", j);
+    return 0;
+}
+
