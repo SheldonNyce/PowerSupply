972fd0016a0f8c4d704a3d1688724a8d27e1689a
SheldonNyce <sheldonnyce@tuks.co.za>
2017-08-20 14:07:57 +0200

    Just testing

--- Main.c
+++ Main.c
@@ -9,17 +9,19 @@
 #include <stdio.h>
 #include "xc.h"
 
-int main(void) 
+int main(void)
 {
-    int i;
-    int j = 0;
-    
-    for (i = 0; i < 100; i++)
-    {
-        j = j + i;
-    }
-        
-    printf("%i", j);
-    return 0;
+  int x = 0;
+
+  int i;
+  int j = 0;
+
+  for (i = 0; i < 100; i++)
+  {
+    j = j + i;
+  }
+
+  printf("%i", j);
+  return 0;
 }
 
