dba77732c548183eae4279b0d997e5920a76fc53
Sheldon Nyce <sheldonnyce@tuks.co.za>
2017-12-16 18:19:35 +0200

    Working flashing LED of DOOM!

--- ConfigBits.h
+++ ConfigBits.h
@@ -1,8 +1,5 @@
-
 // DSPIC33FJ128MC802 Configuration Bit Settings
-
 // 'C' source line config statements
-
 // FBS
 #pragma config BWRP = WRPROTECT_OFF     // Boot Segment Write Protect (Boot Segment may be written)
 #pragma config BSS = NO_FLASH           // Boot Segment Program Flash Code Protection (No Boot program Flash segment)
@@ -19,26 +16,26 @@
 
 // FOSCSEL
 #pragma config FNOSC = FRCPLL           // Oscillator Mode (Internal Fast RC (FRC) w/ PLL)
-#pragma config IESO = ON                // Internal External Switch Over Mode (Start-up device with FRC, then automatically switch to user-selected oscillator source when ready)
+#pragma config IESO = OFF               // Internal External Switch Over Mode (Start-up device with user-selected oscillator source)
 
 // FOSC
 #pragma config POSCMD = NONE            // Primary Oscillator Source (Primary Oscillator Disabled)
 #pragma config OSCIOFNC = OFF           // OSC2 Pin Function (OSC2 pin has clock out function)
-#pragma config IOL1WAY = ON             // Peripheral Pin Select Configuration (Allow Only One Re-configuration)
-#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor (Both Clock Switching and Fail-Safe Clock Monitor are disabled)
+#pragma config IOL1WAY = OFF            // Peripheral Pin Select Configuration (Allow Multiple Re-configurations)
+#pragma config FCKSM = CSECMD           // Clock Switching and Monitor (Clock switching is enabled, Fail-Safe Clock Monitor is disabled)
 
 // FWDT
 #pragma config WDTPOST = PS32768        // Watchdog Timer Postscaler (1:32,768)
 #pragma config WDTPRE = PR128           // WDT Prescaler (1:128)
 #pragma config WINDIS = OFF             // Watchdog Timer Window (Watchdog Timer in Non-Window mode)
-#pragma config FWDTEN = ON              // Watchdog Timer Enable (Watchdog timer always enabled)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (Watchdog timer enabled/disabled by user software)
 
 // FPOR
 #pragma config FPWRT = PWR128           // POR Timer Value (128ms)
 #pragma config ALTI2C = OFF             // Alternate I2C  pins (I2C mapped to SDA1/SCL1 pins)
 #pragma config LPOL = ON                // Motor Control PWM Low Side Polarity bit (PWM module low side output pins have active-high output polarity)
 #pragma config HPOL = ON                // Motor Control PWM High Side Polarity bit (PWM module high side output pins have active-high output polarity)
-#pragma config PWMPIN = ON              // Motor Control PWM Module Pin Mode bit (PWM module pins controlled by PORT register at device Reset)
+#pragma config PWMPIN = OFF             // Motor Control PWM Module Pin Mode bit (PWM module pins controlled by PWM module at device Reset)
 
 // FICD
 #pragma config ICS = PGD1               // Comm Channel Select (Communicate on PGC1/EMUC1 and PGD1/EMUD1)
@@ -48,5 +45,4 @@
 // Use project enums instead of #define for ON and OFF.
 
 #include <xc.h>
-
 
--- Main.c
+++ Main.c
@@ -1,63 +1,52 @@
-/*
- * File:   Main.c
- * Author: andrew
+/* MAGLEV MAIN FILE
+ * File:   main.c
+ * Author: Ben
  *
- * Created on 17 August 2017, 1:45 PM
+ * Created on 20 September 2015, 2:05 PM
  */
-
+#define FCY 20000000UL
 #include <stdio.h>
+#include <stdlib.h>
+#include <libpic30.h>
+#include <xc.h>
+#include <dsp.h>
 #include <p33FJ128MC802.h>
-#include "xc.h"
+#include "ConfigBits.h"
 
-void PWM_Init(void)
+// GLOBAL VARIABLES
+
+int main()
 {
-//    TRISE = 0x00; // make sure PWM pins are set to be outputs
-//    PORTE = 0x00; // clear the outputs 
- 
-    PTCONbits.PTOPS = 0; // PWM timer post-scale
-    PTCONbits.PTCKPS = 3; // PWM timer pre-scale
-    PTCONbits.PTMOD = 0;
- 
-    PTMR = 0; // PWM counter value, start at 0
- 
-    PTPER = 65535; // PWM Time base period
- 
-    PWMCON1bits.PMOD3 = 1; // PWM in complimentary mode
-    PWMCON1bits.PMOD2 = 1; // PWM in complimentary mode
-    PWMCON1bits.PMOD1 = 1; // PWM in complimentary mode
-    PWMCON1bits.PEN3H = 0; // PWM High pin is enabled
-    PWMCON1bits.PEN3L = 0; // PWM Low pin enabled (direction control later?)
-    PWMCON1bits.PEN2H = 0; // PWM High pin is enabled
-    PWMCON1bits.PEN2L = 0; // PWM Low pin enabled (direction control later?)
-    PWMCON1bits.PEN1H = 0; // PWM High pin is enabled
-    PWMCON1bits.PEN1L = 1; // PWM Low pin enabled (direction control later?)
- 
-    //PWMCON2 = 0x0000; // PWM update info
- 
-    DTCON1bits.DTAPS = 0;  //DeadTime pre-scaler
-    DTCON1bits.DTA = 0;   //DeadTime value for 4 us. 
- 
-    //FLTACON = 0x0000; // Fault A Control
- 
-    //OVDCON = 0x0000; // Override control info
- 
-    // Duty Cycle has a max value of 2xPeriod since output  
-    // can change on rising or falling edge of Tcy
-    PDC1 = 32767; // PWM#1 Duty Cycle register (11-bit)
-    PDC2 = 19999; // PWM#2 Duty Cycle register (11-bit)
-    PDC3 = 19999; // PWM#3 Duty Cycle register (11-bit)
+  AD1PCFGL = 0x0; // set all pins to digital except AN0 and AN1 
+  TRISB = 0;
+  TRISA = 0;
+  LATB = 0;
+  LATA = 0;
+  PORTB = 0;
+  PORTA = 0;
 
- //   PLLFBDbits.PLLDIV = 511;
-    
-    PTCONbits.PTEN = 1; // Enable PWM Timer base!
-}
+  unsigned int x = 0;
+  unsigned int y = 0;
 
-int main(void)
-{ 
-  PWM_Init();
-  
   for (;;)
   {
+    PORTA = 0xffff;
+    PORTB = 0xffff;
+
+    for (x = 0; x < 0xfff; x++)
+    {
+      y = y * 2;
+    }
+
+    PORTA = 0x0000;
+    PORTB = 0x0000;
+
+    for (x = 0; x < 0xfff; x++)
+    {
+      y = y * 2;
+    }
   }
+
   return 0;
 }
+
